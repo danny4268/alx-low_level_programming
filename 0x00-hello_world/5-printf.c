ee1bf6928e1a52b661b2686de4aa73a699808d66
danny4268 <ekelemedaniel001@gmail.com>
2022-09-11 10:14:23 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+   * main - print to screen using printf function
+   *
+   * Return: zero
+   */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
