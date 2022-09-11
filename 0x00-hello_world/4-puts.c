fcd2608cb6d234d36d60258091d64680fb3b9a1f
danny4268 <ekelemedaniel001@gmail.com>
2022-09-11 10:06:48 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
+#include <stdio.h>
+/**
+   * main - print to screen using puts function
+   *
+   * Return: zero
+   */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-#!/bin/bash
